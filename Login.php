0c7d261f4f28eea3debc9eb49ad4f9c1043983f5
FrankKamala <frankkamala@gmail.com>
2021-04-12 11:06:01 +0300

    sql file add and headers

--- Login.php
+++ Login.php
@@ -118,10 +118,10 @@ if(isset($_POST['register'])){
 	</script>";
 	}else{
 		
-		$check = "select * from customer where c_email = '$email'";
+		$check = "select * from clients where c_email = '$email'";
 		$run_check = mysqli_query($con,$check);
 		if(mysqli_num_rows($run_check) < 1){
-		$insert_user = "insert into customer(c_session,c_name,c_email,c_pass,c_image) VALUES('$cookie_id','$uname','$email','$pass','default.png')";
+		$insert_user = "insert into clients(c_session,c_name,c_email,c_pass,) VALUES('$cookie_id','$uname','$email','$pass')";
 		$run_insert = mysqli_query($con,$insert_user);
 		if($run_insert){
 			$_SESSION['email'] = $email;
@@ -148,11 +148,11 @@ if(isset($_POST['login'])){
 	$useremail = $_POST['useremail'];
 	$password = $_POST['password'];
 	
-	$check_user = "select * from customer where c_email ='$useremail' && c_pass='$password'";
+	$check_user = "select * from clients where c_email ='$useremail' && c_pass='$password'";
 	$run_user = mysqli_query($con,$check_user);
 	if(mysqli_num_rows($run_user)==1){
 		$_SESSION['email'] = $useremail;
-		$update_session = "update customer set c_session='$cookie_id'";
+		$update_session = "update clients set c_session='$cookie_id'";
 		$run_update = mysqli_query($con,$update_session);
 		
 	    echo "<script>window.open('index.php?login','_self')</script>";
---
+++ crud.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `clients` 
+( `c_id` int(250) NOT NULL,
+ `c_session` varchar(200) NOT NULL,
+  `c_name` varchar(200) NOT NULL, 
+  `c_email` varchar(200) NOT NULL,
+   `c_pass` varchar(200) NOT NULL 
+   )
