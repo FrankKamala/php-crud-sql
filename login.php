652c26b11eba241882707af09378ee3f148c2896
FrankKamala <frankkamala@gmail.com>
2021-04-14 00:15:48 +0300

    login and logout

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+?>
+
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+
+   <div class="container">  
+    <div class="row">  
+        <div class="col-md-4 col-md-offset-4">  
+            <div class="login-panel panel panel-primary">  
+                <div class="panel-heading">  
+                    <h3 class="panel-title">Sign In</h3>  
+                </div>  
+                <div class="panel-body">
+				
+       
+        
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+         </div>  
+            </div>  
+        </div>  
+    </div>  
+</div>   
+</body>
+</html>
