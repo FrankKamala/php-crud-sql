f53bb9175a6977a18b06eb69e2ff95d0b20a046a
FrankKamala <frankkamala@gmail.com>
2021-04-14 10:23:17 +0300

    change nav andlog out link

--- available.php
+++ available.php
@@ -16,6 +16,57 @@
   href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css"
   rel="stylesheet"
 />
+
+ <!-- Navbar -->
+ <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+      <img
+        src="https://image.pngaaa.com/208/906208-middle.png"
+        height="30"
+        alt=""
+        loading="lazy"
+      />
+
+      </a>
+      
+    </div>
+    <!-- Item -->
+    <div class="container">
+      <a class="navbar-brand" href="#">Home</a>
+    </div>
+    <div class="container">
+      <a class="navbar-brand" href="#">About Us</a>
+    </div>
+
+    <!-- icon -->
+    <div class="container-fluid">
+      <ul class ="navbar-nav d-flex flex-row">
+      <li class="nav-item me-3 me-lg-0 dropdown">
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="fas fa-user"></i>
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="logout.php">Log Out</a></li>
+         
+          <li><hr class="dropdown-divider" /></li>
+          
+        </ul>
+      </li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+
 <div class="row">
   <div class="col-sm-6">
     <div class="card">
@@ -42,3 +93,9 @@
     </div>
   </div>
 </div>
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"
+></script>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 
-
 <!-- Font Awesome -->
 <link
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
